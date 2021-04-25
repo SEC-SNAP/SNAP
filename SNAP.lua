c9a663f8316be4471923ae5a04c9c0be0973f293
SEC-SNAP <80710912+SEC-SNAP@users.noreply.github.com>
2021-04-25 22:20:22 +0300

    Add files via upload

--- SNAP.lua
+++ SNAP.lua
@@ -2601,13 +2601,17 @@ end
 end,nil)   
 end
 -----------
-if text == 'السورس' or text == 'سورس' or text == 'ياسورس' or text == 'يا سورس' then       
+if text == 'السورس' or text == 'سورس' or text == 'ياسورس' then       
 local Text ='𓄼𝙎𝙤𝙐𝙧𝙎𝙚 𝙎𝙣𝙖𝙥𓄹'
 keyboard = {} 
 keyboard.inline_keyboard = {
+{
 {{text =  '𝘾𝙝𝙖𝙣𝙣𝙚𝙡 𝙎𝙣𝙖𝙋',url="t.me/"iinzzz}},
+},
+{
 {{text =  '𝘿𝙚𝙫 𝙎𝙣𝙖𝙥',url="t.me/Z22SS"}},
-}
+},
+{
 local msg_id = msg.id_/2097152/0.5
 https.request("https://api.telegram.org/bot"..token..'/sendMessage?chat_id=' .. msg.chat_id_ .. '&text='..URL.escape(Text).."&reply_to_message_id="..msg_id.."&parse_mode=markdown&disable_web_page_preview=true&reply_markup="..JSON.encode(keyboard))
 end
@@ -10208,7 +10212,7 @@ local List = {
 ┌ 𝐔𝐒𝐄𝐑 𖤱 #username 𖦴 .
 ├ 𝐌𝐒𝐆 𖤱 #msgs 𖦴 .
 ├ 𝐒??𝐀 ?? #stast 𖦴 .
-└ 𝐈𝐃 𖤱 #id 𖦴 .
+└ 𝐈𝐃 ?? #id 𖦴 .
 ]],
 [[
 𓄼🇮🇶 𝑼𝒔𝒆𝒓𝑵𝒂𝒎𝒆 :#username 
